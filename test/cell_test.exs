51642ee5a8898ba5deb115729aab63b3e7790344
João Otávio Biondo <joao.o.b@hotmail.com>
2020-05-05 20:35:19 -0300

    using describe to test Cell.to_string/1

--- test/cell_test.exs
+++ test/cell_test.exs
@@ -5,18 +5,22 @@ defmodule GameOfLife.CellTest do
 
   test "cell should not be alive when created" do
     cell = %Cell{}
+
     assert cell.alive == false
   end
 
-  test "to_string/1 when dead" do
-    cell = %Cell{}
+  describe "Cell.to_string/1" do
+    test "when dead" do
+      cell = %Cell{}
 
-    assert Cell.to_string(cell) == "   "
+      assert Cell.to_string(cell) == "   "
+    end
+
+    test "when alive" do
+      cell = %Cell{alive: true}
+
+      assert Cell.to_string(cell) == " x "
+    end
   end
 
-  test "to_string/1 when alive" do
-    cell = %Cell{alive: true}
-
-    assert Cell.to_string(cell) == " x "
-  end
 end
