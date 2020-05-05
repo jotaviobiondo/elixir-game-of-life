d6cc71b96b4f7e8c3e303754e6833d10d83ebc06
João Otávio Biondo <joao.o.b@hotmail.com>
2020-05-05 20:29:39 -0300

    adding tests for CLI module

--- lib/cli.ex
+++ lib/cli.ex
@@ -3,15 +3,15 @@ defmodule GameOfLife.CLI do
   @grid_size_default 10
 
   @default_options %{
-    :generations => @generations_default,
-    :grid_size => @grid_size_default
+    generations: @generations_default,
+    grid_size: @grid_size_default
   }
 
   def main(args) do
     args |> parse_args() |> start_game_of_life()
   end
 
-  defp parse_args(args) do
+  def parse_args(args) do
     {options, _, _} =
       OptionParser.parse(args,
         strict: [generations: :integer, grid_size: :integer],
@@ -21,11 +21,11 @@ defmodule GameOfLife.CLI do
     Enum.into(options, @default_options)
   end
 
-  def start_game_of_life(%{generations: generations, grid_size: grid_size}) do
+  defp start_game_of_life(%{generations: generations, grid_size: grid_size}) do
     GameOfLife.start(generations, grid_size)
   end
 
-  def start_game_of_life(_) do
+  defp start_game_of_life(_) do
     IO.puts("Invalid input")
   end
 end
---
+++ test/cli_test.exs
@@ -0,0 +1,26 @@
+defmodule GameOfLife.CLITest do
+  use ExUnit.Case
+  alias GameOfLife.CLI
+  doctest GameOfLife.CLI
+
+  describe "Cell.parse_args/1" do
+    test "with all args" do
+      options = CLI.parse_args(["--generations", "50", "--grid-size", "100"])
+
+      assert options == %{generations: 50, grid_size: 100}
+    end
+
+    test "with no args must have default values" do
+      options = CLI.parse_args([])
+
+      assert Map.keys(options) == [:generations, :grid_size]
+    end
+
+    test "with 1 argument and others must have default values" do
+      options = CLI.parse_args(["--generations", "50"])
+
+      assert Map.keys(options) == [:generations, :grid_size]
+      assert %{generations: 50} = options
+    end
+  end
+end
