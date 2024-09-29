c338c6d84375bafecd8dafbd1f23962a77edba15
João Otávio Biondo <joao.otb@gmail.com>
2024-09-28 21:01:59 -0300

    Improve CLI to print grid clearing the previous lines

--- lib/game_of_life_cli/cli.ex
+++ lib/game_of_life_cli/cli.ex
@@ -11,7 +11,9 @@ defmodule GameOfLifeCLI.CLI do
 
   @spec main([String.t()]) :: no_return
   def main(args) do
-    args |> parse_args() |> start_game_of_life()
+    args
+    |> parse_args()
+    |> start_game_of_life()
   end
 
   @spec parse_args([String.t()]) :: %{generations: pos_integer, grid_size: pos_integer}
@@ -25,13 +27,32 @@ defmodule GameOfLifeCLI.CLI do
     Enum.into(options, @default_options)
   end
 
-  defp start_game_of_life(%{generations: generations, grid_size: grid_size}) do
-    Life.get_random_stream(grid_size)
-    |> Stream.take(generations)
+  defp start_game_of_life(%{generations: max_generations, grid_size: grid_size}) do
+    information_lines = 1
+    borders_top_and_bottom = 2
+    number_of_lines_printed = grid_size + borders_top_and_bottom + information_lines
+
+    grid_size
+    |> Life.get_random_stream()
+    |> Stream.take(max_generations)
     |> Stream.map(&to_string/1)
-    |> Enum.each(fn grid_str ->
-      IO.write(grid_str)
+    |> Stream.with_index(1)
+    |> Enum.each(fn {grid_str, current_generation} ->
+      grid_str |> with_color(:blue) |> IO.write()
+      "Generation: #{current_generation}/#{max_generations}" |> with_color(:yellow) |> IO.puts()
+
       Process.sleep(100)
+
+      last_generation? = current_generation == max_generations
+
+      if not last_generation? do
+        for _i <- 1..number_of_lines_printed, do: clear_line()
+      end
     end)
   end
+
+  defp with_color(text, :blue), do: IO.ANSI.blue() <> text <> IO.ANSI.reset()
+  defp with_color(text, :yellow), do: IO.ANSI.yellow() <> text <> IO.ANSI.reset()
+
+  defp clear_line, do: IO.write(IO.ANSI.cursor_up(1) <> IO.ANSI.clear_line())
 end
